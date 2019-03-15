b8817995acff17e9ea2e0aafe1455f62fdbca55e
Donny Fitrado <36808083+mbohlahdon@users.noreply.github.com>
2019-03-15 19:06:14 +0700

    Update soal5b.c

--- soal5/soal5b.c
+++ soal5/soal5b.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
 
 int main(){
-	char *argv[3] = {"killall", "soal5a", NULL};
-	execv("/usr/bin/killall", argv);
-	
+    char out[7]={0};
+    FILE *cmd = popen("pidof /home/hp/Documents/sisop2/soal5/soal5a", "r");
+    fgets(out, 7, cmd);
+    pid_t pid = strtoul(out, NULL, 10);
+    pclose(cmd);
+
+    if(!kill(pid, SIGKILL))
+        printf("Process with pid of %d is successfully killed\n", pid);
+
+    return 0;
 }
